978630ce103feceb3ac4b7c7180ad4e518255d5e
Mohamed Firas Alfarra <fralfarra11@gmail.com>
2025-12-10 12:53:18 +0300

    uplode

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
+import react from "@vitejs/plugin-react";
+import tailwind from "tailwindcss";
+import { defineConfig } from "vite";
+import path from "path";
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  publicDir: "./static",
+  base: "./",
+  css: {
+    postcss: {
+      plugins: [tailwind()],
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
   },
 });
